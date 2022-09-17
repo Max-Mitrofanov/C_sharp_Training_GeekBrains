02be8062fa3b124d19a13a0f139a1c1a9489e55f
Maksim Mitrofanov <mvmitrofanov17@gmail.com>
2022-09-17 11:47:57 +0300

    FavoritePersonGreetings

---
+++ Example005_FavoritePerson/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.Write("Enter user name: " );
+string username = Console.ReadLine();
+
+if(username.ToLower() == "masha"){
+    Console.WriteLine("Horey! This is Masha! =)");
+} else {
+    Console.WriteLine("Oh, it's you again =(");
+};
