5f4388a4ef4cfc7409812811c239883b756183ae
Maksim Mitrofanov <mvmitrofanov17@gmail.com>
2022-10-10 13:53:18 +0600

    HW_6_GB

---
+++ HW/HW_6_C_Sharp_GB/Task41/Program.cs
@@ -0,0 +1,27 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры число M,
+// потом вводит M чисел ЧЕРЕЗ ENTER.
+// Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+Console.Clear();
+
+Console.Write("How many numbers do you want to enter? ");
+int numberCount = int.Parse(Console.ReadLine());
+
+int[] createArr (){
+    int[] arr = new int[numberCount];
+    for (int i = 0; i < arr.Length; i++){
+        Console.Write("Enter a random number: ");
+        arr[i] = int.Parse(Console.ReadLine());
+    }
+    return arr;
+}
+
+int countPositive (int[] arr) {
+    int count = 0;
+    for (int i = 0; i < arr.Length; i++){
+        if (arr[i] > 0) count++;
+    }
+    return count;
+}
+
+Console.WriteLine("Numbers above zero: " + countPositive(createArr()));
---
+++ HW/HW_6_C_Sharp_GB/Task43/Program.cs
@@ -0,0 +1,31 @@
+﻿// Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых,
+// заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+
+Console.Clear();
+Console.Write("Введите угловой коэффициент k для первой прямой: ");
+int k1 = int.Parse(Console.ReadLine());
+Console.Write("Введите угловой коэффициент k для второй прямой: ");
+int k2 = int.Parse(Console.ReadLine());
+Console.Write("Введите свободный коэффициент b для первой прямой: ");
+int b1 = int.Parse(Console.ReadLine());
+Console.Write("Введите свободный коэффициент b для второй прямой: ");
+int b2 = int.Parse(Console.ReadLine());
+
+double[] calcIntersection (double k1, double k2, double b1, double b2){
+
+    double x = (b2 - b1) / (k1 - k2);
+    double y = k2 * x + b2;
+    double[] arr = {x,y};
+    return arr;
+} 
+
+void printArr(double[] arr){
+    Console.Write("[ ");
+    for (int i = 0; i < arr.Length; i++){
+        if (i < arr.Length - 1) Console.Write($"{arr[i]}, ");
+        else Console.Write($"{arr[i]}");
+    }
+    Console.Write(" ] ");
+}
+
+printArr(calcIntersection(k1, k2, b1, b2));
